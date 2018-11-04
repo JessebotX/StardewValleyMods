7e50466901b684387c04b066dfe7770c8041424e
JessebotX <43657800+JessebotX@users.noreply.github.com>
2018-11-04 09:08:12 -0800

    xD lol

---
+++ BiggerRiverlandsFarm/BiggerRiverlandsFarm/ModConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+
+namespace BiggerRiverlandsFarm
+{
+    class ModConfig
+    {
+        public bool ProgressionMap { get; set; } = true;
+    }
+}
---
+++ BiggerRiverlandsFarm/BiggerRiverlandsFarm/ModEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace BiggerRiverlandsFarm
+{
+
+    public class ModEntry : Mod, IAssetLoader
+    {
+        private ModConfig Config;
+        
+
+        public bool CanLoad<T>(IAssetInfo asset)
+        {
+            return asset.AssetNameEquals("Maps/Farm_Fishing");
+        }
+
+        public override void Entry(IModHelper helper)
+        {
+            Config = this.Helper.ReadConfig<ModConfig>();
+        }
+
+        T IAssetLoader.Load<T>(IAssetInfo asset)
+        {
+            if (this.Config.ProgressionMap)
+            {
+                return this.Helper.Content.Load<T>("assets/Farm_Fishing.tbin", ContentSource. ModFolder);
+            }
+
+            else
+            {
+                return this.Helper.Content.Load<T>("assets/Farm_Fishing_ProgressionMap.tbin", ContentSource. ModFolder);
+            }
+        }
+    }
+}
