36ee798c24936e03f0dafc514610f26520113553
JessebotX <43657800+JessebotX@users.noreply.github.com>
2019-01-10 18:55:20 -0800

    wip

--- [WIP]/Sprint/Sprint/ModEntry.cs
+++ [WIP]/Sprint/Sprint/ModEntry.cs
@@ -18,7 +18,7 @@ namespace Sprint
 
         public override void Entry (IModHelper helper)
         {
-            /* Button Pressed Event Handler */
+            /* Event Handler */
             helper.Events.Input.ButtonPressed += this.OnButtonPressed;
 
             /* Read Config */
@@ -27,6 +27,7 @@ namespace Sprint
 
         private void OnButtonPressed (object sender, ButtonPressedEventArgs e)
         {
+            // if player isn't free to act in the world, do nothing
             if (!Context.IsPlayerFree)
             {
                 return;
@@ -34,16 +35,20 @@ namespace Sprint
 
             else
             {
+                // suppress game keybinds depending on config values
                 this.Helper.Input.Suppress(this.Config.PrimarySprintKey);
                 this.Helper.Input.Suppress(this.Config.SecondarySprintKey);
                 this.Helper.Input.Suppress(this.Config.ControllerSprintButton);
+                this.Helper.Input.Suppress(this.Config.SlowDownKey);
 
+                // is the key/button being pressed
                 bool isPrimarySprintKeyPressed = this.Helper.Input.IsDown(this.Config.PrimarySprintKey);
                 bool isSecondarySprintKeyPressed = this.Helper.Input.IsDown(this.Config.SecondarySprintKey);
                 bool isControllerSprintButtonPressed = this.Helper.Input.IsDown(this.Config.ControllerSprintButton);
+
                 if (isPrimarySprintKeyPressed || isSecondarySprintKeyPressed || isControllerSprintButtonPressed)
                 {
-                    Game1.player.addedSpeed = 2;
+                    Game1.player.addedSpeed = 3;
                 }
             }
         }
