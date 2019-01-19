4265bbf41749e357bd2041b7e92af648d7e8d9a3
JessebotX <43657800+JessebotX@users.noreply.github.com>
2019-01-18 17:36:56 -0800

    wip

--- [WIP]/Sprint/Sprint/ModEntry.cs
+++ [WIP]/Sprint/Sprint/ModEntry.cs
@@ -17,13 +17,37 @@ namespace Sprint
         //reference ModConfig class
         private ModConfig Config;
 
+        private bool playerSprinting = false;
+        private int sprintSpeed = 0;
+        private int secondsUntilIncrement = 4;
+
         public override void Entry(IModHelper helper)
         {
             /* Event Handlers */
             helper.Events.GameLoop.UpdateTicked += this.UpdateTicked;
+            helper.Events.GameLoop.OneSecondUpdateTicked += this.OneSecond;
 
             /* Read Config */
             this.Config = helper.ReadConfig<ModConfig>();
+        }
+
+        private void OneSecond(object sender, OneSecondUpdateTickedEventArgs e)
+        {
+            if (!Context.IsPlayerFree)
+            {
+                return;
+            }
+
+            else
+            {
+                if (playerSprinting)
+                {
+                    if (secondsUntilIncrement > 0)
+                    {
+                        secondsUntilIncrement--;
+                    }
+                }
+            }
         }
 
         private void UpdateTicked(object sender, UpdateTickedEventArgs e)
@@ -39,7 +63,21 @@ namespace Sprint
                 bool sprintKeyPressed = this.Helper.Input.IsDown(this.Config.SprintKey);
                 if (sprintKeyPressed)
                 {
-                    Game1.player.addedSpeed = 4;
+                    playerSprinting = true;
+                    Game1.player.Speed += sprintSpeed;
+
+                    if (secondsUntilIncrement == 4 )
+                    {
+                        sprintSpeed = 3;
+                    }
+                    else if (secondsUntilIncrement <= 3 && secondsUntilIncrement > 1)
+                    {
+                        sprintSpeed = 4;
+                    }
+                    else if (secondsUntilIncrement <= 1)
+                    {
+                        sprintSpeed = 5;
+                    }
                 }
             }
         }
