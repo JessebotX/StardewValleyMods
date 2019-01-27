c00ac5775a87f478fd08bd03fc85ee2bebf4005b
JessebotX <43657800+JessebotX@users.noreply.github.com>
2019-01-27 15:51:31 -0800

    internal

---
+++ [internal]/Starvation/Starvation/ModEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewValley;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Framework;
+using StardewModdingAPI.Utilities;
+
+namespace Starvation
+{
+    class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+        }
+    }
+}
